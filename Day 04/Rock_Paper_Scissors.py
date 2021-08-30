15f13f2d71e2ccdf50aa7fbe1e97a35966f3a309
Bruno Daniel <bruno.daniel@outlook.com>
2021-08-30 15:31:45 -0300

    Day 04

---
+++ Day 04/Rock_Paper_Scissors.py
@@ -0,0 +1,9 @@
+# askpython.com
+# Random
+import random
+#import my_module
+random_int = random.randint(1, 10)
+print(random_int)
+# print(my_module.pi)
+random_float = random.random()*5
+print(random_float)
