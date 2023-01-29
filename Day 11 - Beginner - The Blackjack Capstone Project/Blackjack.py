f2553fbb02136b18723f15e21ea5ed8643ea249a
Bruno Daniel <bruno.daniel@outlook.com>
2023-01-29 20:38:51 -0300

    Update Blackjack.py

--- Day 11 - Beginner - The Blackjack Capstone Project/Blackjack.py
+++ Day 11 - Beginner - The Blackjack Capstone Project/Blackjack.py
@@ -2,18 +2,14 @@ import os
 import random
 from art import logo
 cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
-player = 0
-dealer = 0
+player_score = 0
+dealer_score = 0
 player_hand = []
 dealer_hand = []
 
 def clear():
     return os.system('cls||clear')
-# def check_hands(player_hand, dealer_hand):
-#     if player_hand == 21 and dealer_hand < 21:
-#         return True
-#     elif player_hand < dealer_hand:
-#         return False
+
     
 def draw_card():
     return cards[random.randint(0, len(cards))]
@@ -22,8 +18,8 @@ def first_draw():
     player_hand.append(draw_card())
     player_hand.append(draw_card())
     dealer_hand.append(draw_card())
-    player = sum(player_hand)
     
+
 
 def blackjack():    
     
@@ -31,13 +27,15 @@ def blackjack():
     while should_continue:
         clear()
         print(logo)   
-        
-        print(player_hand)
+        first_draw()
+        player_score = sum(player_hand)
+        print(f"Your cards: {player_hand}, current score: {player_score}")
+        print(f"Computer first card: {dealer_hand}")
         if input("New game?: 'y' or 'n'").lower() == 'y':
             print("again")
         else:
             print("bye")
             should_continue = False
-            #blackjack()
+            
             
 blackjack()
